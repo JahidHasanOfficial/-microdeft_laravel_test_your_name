0c136af863b7f4b5d99e1818ff82cd83217c1ee9
Jahid Hasan <jahidhasanofficial23@gmail.com>
2024-12-23 16:43:10 +0600

    project

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
-use App\Http\Controllers\RequisitionController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', [RequisitionController::class, 'index'])->name('home');
-Route::get('/requisitions', [RequisitionController::class, 'index'])->name('requisitions.index');
-Route::get('/requisitions/create', [RequisitionController::class, 'create'])->name('requisitions.create');
-Route::post('/requisitions', [RequisitionController::class, 'store'])->name('requisitions.store');
-Route::get('/requisitions/{id}', [RequisitionController::class, 'show'])->name('requisitions.show');
-Route::get('/requisitions/{id}/edit', [RequisitionController::class, 'edit'])->name('requisitions.edit');
-Route::put('/requisitions/{id}', [RequisitionController::class, 'update'])->name('requisitions.update');
-Route::delete('/requisitions/{id}', [RequisitionController::class, 'destroy'])->name('requisitions.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
